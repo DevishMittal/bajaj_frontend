12200eb27dc958e28c1b9367a141aa5e2bbabcab
Devish Mittal <devishmittal1@gmail.com>
2025-04-25 17:33:47 +0530

    add remote image patterns for doctor listing integration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'doctorlistingingestionpr.azureedge.net',
+        port: '',
+        pathname: '/**', // Allow any path under this hostname
+      },
+      {
+        protocol: 'https',
+        hostname: 'doctorlistingingestionpr.blob.core.windows.net',
+        port: '',
+        pathname: '/**', // Also allow the blob storage hostname seen in the API data
+      },
+    ],
+  },
 };
 
 export default nextConfig;
